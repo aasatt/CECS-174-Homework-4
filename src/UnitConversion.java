86e1913a162cb1012ded5013b522b0a4fe98053d
aasatt <aasatt@gmail.com>
2015-03-14 12:29:08 -0700

    Shower Thought to simplify #4

--- src/UnitConversion.java
+++ src/UnitConversion.java
@@ -14,7 +14,20 @@
  * if you have questions feel free to text me (714) 465-6287
  * 
  * 
+ * 
+ * 
+ * 
+ * 
+ * ***UPDATE***
+ * 	I was in the shower and had a though about how i could reduce the code by 1/3rd so here it is... still seem a little repetitive for me but better than
+ * it was before 
+ * 
+ * 
  */
+
+
+
+
 public class UnitConversion {
 	public static void main (String[] args) {
 		//ask user covert from? (fl.oz, gal, oz, lb, in, ft, mi)
@@ -23,116 +36,99 @@ public class UnitConversion {
 		//now i would init the Converter class with the first value
 		// call secondValueValid from Converter class like this
 		
-//		boolean valid = false;
-//		while (!valid) {
-//			valid = secondValueValid();//this will keep running until valid second value
+//		int reult = -1;
+//		while (result == -1) {
+//			result = getResult();//this will keep running until valid second value
 //		}
 
-		//ask user for value to convert and pass it to getResult();
-		//display result 
+		//display result to user
 	}
 }
 
 
 class Converter{
 	//declare private vars here
-	//i would have a private int called 
-	
-	
-/*
- * KEEP THIS COMMENT	
- * Combo will eventually be set with a value to represent this: 
-				// 1 = floz -> ml
-				// 2 = floz -> l
-				// 3 = gal -> ml
-				// 4 = gal -> l
-				// 5 = oz -> g
-				// 6 = oz -> kg
-				// 7 = lb -> g
-				// 8 = lb -> kg
-				// 9 = in -> mm
-				// 10 = in -> cm
-				// 11 = in -> m
-				// 12 = in -> km
-				// 13 = ft -> mm
-				// 14 = ft -> cm
-				// 15 = ft -> m
-				// 16 = ft -> km
-				// 17 = mi -> mm
-				// 18 = mi -> cm
-				// 19 = mi -> m
-				// 20 = mi -> km
-*/
 
 	public Converter (String from) {
 		//set private var to the one passed in
 	}
-	public boolean secondValueValid() {
+	public double getResult() {
+		//double conversion.... this will be the multiplier you are using to actually do the conversion
 		//ask user covert to? (ml, l, g, kg, mm, cm, m, km).
 		//set string value - nextLine();
-		//use if else statments to set the value of combo to the correct one and return true if valid 
+		//use if else statments to find a matching combo
+		
+		/*
+		 * KEEP THIS COMMENT	
+		 *Here all all the combos possible
+						// 1 = floz -> ml
+						// 2 = floz -> l
+						// 3 = gal -> ml
+						// 4 = gal -> l
+						// 5 = oz -> g
+						// 6 = oz -> kg
+						// 7 = lb -> g
+						// 8 = lb -> kg
+						// 9 = in -> mm
+						// 10 = in -> cm
+						// 11 = in -> m
+						// 12 = in -> km
+						// 13 = ft -> mm
+						// 14 = ft -> cm
+						// 15 = ft -> m
+						// 16 = ft -> km
+						// 17 = mi -> mm
+						// 18 = mi -> cm
+						// 19 = mi -> m
+						// 20 = mi -> km
+		*/
+		
+		
 		
 //		if (from == "fl.oz") {
 //			if (to == "ml") {
-//				combo = 1;
-//				return true;
+//				conversion = 29.5735;
+//				//ask user for the value to convert
+//				return (value*conversion);
 //			} else if (to == "l") {
-//				combo = 2;
-//				return true;
+//				conversion = .0295735;
+//				//ask user for the value to convert
+//				return (value*conversion);
 //			}
 //			
 //			
 //			
 //		} else if (from == "gal") {
 //			if (to == "ml") {
-//				combo = 3;
-//				return true;
+//				conversion = 3785.41;
+//				/ask user for the value to convert
+//				return (value*conversion);
 //			} else if (to == "l") {
-//				combo = 4;
-//				return true;
+//				conversion = 3.78541;
+//				//ask user for the value to convert
+//				return (value*conversion);
 //			}
 //			
 //			
 //			
 //		} else if (from == "oz") {
 //			if (to == "g") {
-//				combo = 1;
-//				return true;
+//				conversion = 28.3495;
+//				//ask user for the value to convert
+//				return (value*conversion);
 //			} else if (to == "kg") {
-//				combo = 2;
-//				return true;
-//			}
-//			
-//			
-//			
-//		} else if (from == "lb") {
-//			if (to == "g") {
-//				combo = 3;
-//				return true;
-//			} else if (to == "kg") {
-//				combo = 4;
-//				return true;
-//			}
-//		}
-//		
-//		
+//				conversion = 0.0283495;
+//				//ask user for the value to convert
+//				return (value*conversion);
+//			} 
+		
+		
 		//..............and so on
 		
 		
-		//if the combo never is valid it will eventually return false 
+		//if the combo never is valid it will eventually return -1 which will restart the while loop in main 
 		
-		return false; //return the result
+		return -1; //return the result
 	}
-	public double getResult() {
-		//check the combo and convert
-//		if (combo == 1) {
-//			//return (from*29.5735)
-//		} else if (combo == 2) {
-//			return conversion for fl.oz to l (from*.0295735)
-//			
-//		} //.....and so on for every combo
-//		
-//		
-		return -1; //this should never get called
-	}
+
 }
