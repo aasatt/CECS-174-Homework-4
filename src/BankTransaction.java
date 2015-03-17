c40661b49b053c97474ca6d285963b01eee209f1
aasatt <aasatt@gmail.com>
2015-03-16 18:29:16 -0700

    Finished #3

--- src/BankTransaction.java
+++ src/BankTransaction.java
@@ -9,13 +9,43 @@ import java.util.Scanner;
 
 public class BankTransaction {
 	public static void main (String[] args) {
-		Scanner input = new Scanner(System.in);
-		double checkingBal = 0;
-		double savingsBal = 0;
-		double inDouble = -1;
 		ATMControl atm = new ATMControl();
 		atm.setBalances();
-		
+		Scanner input = new Scanner(System.in);
+		System.out.print("*******************************\n*                             *\n*  How can I help you today?  *\n*                             * \n*******************************\n1. Deposit\n2.Withdraw\n3.Transfer\n\nEnter option number: ");
+		switch(input.nextInt()) {
+		 	case 1:  {
+		 		System.out.print("\nFor what account?\n1.Checking\n2.Savings\nEnter option number: ");
+		 		int account = input.nextInt();
+		 		System.out.print("\nEnter deposit amount: ");
+		 		double amount = input.nextDouble();
+		 		atm.deposit(account, amount);
+		 	}
+         		break;
+		 	case 2:  {
+		 		System.out.print("\nFor what account?\n1.Checking\n2.Savings\nEnter option number: ");
+		 		int account = input.nextInt();
+		 		boolean success = false;
+		 		while (!success) {
+		 			System.out.print("\nEnter withdraw amount: ");
+		 			double amount = input.nextDouble();
+		 			success = atm.withdraw(account, amount);
+		 		}
+		 	}
+		 		break;
+		 	case 3:  {
+		 		System.out.print("\nInto what account would you like to transfer to?\n1.Checking\n2.Savings\nEnter option number: ");
+		 		int account = input.nextInt();
+		 		boolean success = false;
+		 		while (!success) {
+			 		System.out.print("\nEnter transfer amount: ");
+			 		double amount = input.nextDouble();
+			 		success = atm.transfer(account, amount);
+		 		}
+		 	}
+         		break;
+		}
+		atm.getBalances();
 	}
 }
 
@@ -47,6 +77,48 @@ class ATMControl {
 				System.out.println("Balance must not be negative.");
 			}
 		}	
+	}
+	public void deposit(int account, double amount) {
+		account--;
+		checkingBal = (account == 0) ? checkingBal + amount : checkingBal;
+		savingsBal = (account == 1) ? savingsBal + amount : savingsBal;
+	}
+	public boolean withdraw(int account, double amount) {
+		account--;
+		double currentAccount = (account == 0) ? checkingBal : savingsBal;
+		if (amount > currentAccount) {
+			System.out.println("You must not overdraft this account!");
+			return false;
+		}
+		
+		if (account == 0){
+			checkingBal = currentAccount-amount;
+		} else {
+			savingsBal = currentAccount-amount;
+		}
+		return true;
+	}
+	public boolean transfer(int account, double amount) {
+		account--;
+		double currentAccount = (account == 0) ? savingsBal : checkingBal;
+		if (amount > currentAccount) {
+			System.out.println("You must not overdraft this account!");
+			return false;
+		}
+		System.out.println(account +" this account");
+		if (account == 0){
+			checkingBal = checkingBal+amount;
+			savingsBal = savingsBal-amount;
+		} else {
+			savingsBal = savingsBal+amount;
+			checkingBal = checkingBal-amount;
+		}
+		return true;
+		
+	}
+	public void getBalances(){
+		System.out.print("Checking Balance: $"+checkingBal+" Savings Balance: $"+savingsBal);
+
 	}
 
 }
